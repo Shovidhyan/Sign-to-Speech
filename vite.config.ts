106385e6a02eef013348ae4d0e152fb8bd28ea1f
Shovidhyan <shovidhyan77@gmail.com>
2025-05-03 08:47:43 +0530

    Initial commit - pushed project to new repo1

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Indian-Sign-Language/', // Must match GitHub repo name exactly
   plugins: [react()],
-  base: '/Indian-Sign-Language/' // 👈 Add this line
-})
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
