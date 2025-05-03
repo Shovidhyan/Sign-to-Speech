7e89d70ae46d014fe6da619bbcd8fc5bd65f3a9a
Shovidhyan <shovidhyan77@gmail.com>
2025-05-03 08:43:23 +0530

    Your latest update message

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Indian-Sign-Language-Translator/", // GitHub repo name
-  
   plugins: [react()],
-  
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-    
-  },
-});
+  base: '/Indian-Sign-Language/' // 👈 Add this line
+})
