84cba6d54351af07e29fa9a2403e58fcfa8fa88a
Shovidhyan <shovidhyan77@gmail.com>
2025-05-03 08:36:15 +0530

    Initial commit - pushed project to new repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Indian-Sign-Language-Translator/", // GitHub repo name
+  
   plugins: [react()],
+  
   optimizeDeps: {
     exclude: ['lucide-react'],
+    
   },
 });
